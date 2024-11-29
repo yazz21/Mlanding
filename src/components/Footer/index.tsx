b36b3a897b0077fa4e61e54a3ba639d4228470f6
yazz21 <joo21safe@gmail.com>
2024-11-29 11:57:32 +0300

    fix: footer logo fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -131,33 +131,22 @@ const Footer = ({ t }: { t: TFunction }) => {
             </NavLink>
             <FooterContainer>
               <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
+                href="https://github.com"
                 src="github.svg"
               />
               <SocialLink
-                href="https://twitter.com/Adrinlolx"
+                href="https://twitter.com/"
                 src="twitter.svg"
               />
+                <SocialLink
+                  href="https://medium.com/"
+                  src="medium.svg"
+                />
               <SocialLink
-                href="https://www.linkedin.com/in/lasha-kakabadze/"
+                href="https://www.linkedin.com/"
                 src="linkedin.svg"
               />
-              <SocialLink
-                href="https://medium.com/@lashakakabadze/"
-                src="medium.svg"
-              />
-              {/* <a
-                href="https://ko-fi.com/Y8Y7H8BNJ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  height="36"
-                  style={{ border: 0, height: 36 }}
-                  src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                  alt="Buy Me a Coffee at ko-fi.com"
-                />
-              </a> */}
+            
             </FooterContainer>
           </Row>
         </Container>
